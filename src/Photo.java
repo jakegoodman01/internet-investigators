aad9a239c6ea0a774a857d5bfc7894a9e24dae39
Jake Goodman <jakeg135@gmail.com>
2018-05-16 10:49:49 -0400

    Implemented Caption class into Photo.jav

--- src/Photo.java
+++ src/Photo.java
@@ -1,35 +1,40 @@
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
-public class Photo extends ImageView {
+public class Photo extends HBox {
+    private ImageView imageView;
     private Image image;
-    private String caption;
+    private Caption caption;
 
     public Photo(String link) {
         super();
         image = new Image(link);
-        super.setImage(image);
-        super.setPreserveRatio(true);
+        imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        this.getChildren().addAll(imageView);
     }
 
     public Photo(String link, int height) {
         super();
         image = new Image(link);
-        super.setImage(image);
-        super.setPreserveRatio(true);
-        super.setFitHeight(height);
+        imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        imageView.setFitHeight(height);
+        this.getChildren().addAll(imageView);
     }
 
     public Photo(String link, int height, String caption) {
         super();
         image = new Image(link);
-        super.setImage(image);
-        super.setPreserveRatio(true);
-        super.setFitHeight(height);
-        this.caption = caption;
+        imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        imageView.setFitHeight(height);
+        this.caption = new Caption(caption, imageView.getFitHeight());
+        this.getChildren().addAll(imageView, this.caption);
     }
 
-    public String getCaption() {
+    public Caption getCaption() {
         return caption;
     }
 }
