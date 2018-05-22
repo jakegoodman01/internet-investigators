4a823fa566fbcd315af5f3844d96df16cbf7a640
Jake Goodman <jakeg135@gmail.com>
2018-05-22 11:51:05 -0400

    Populates timeline with only PicturePosts

--- src/LevelBuilder.java
+++ src/LevelBuilder.java
@@ -1,10 +1,11 @@
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Stream;
 
 
 public class LevelBuilder {
@@ -19,6 +20,9 @@ public class LevelBuilder {
             String name = "";
             String bio = "";
             boolean readBio = false;
+            boolean picturePost = false;
+            String photoLink = null;
+            Post post = null;
             while ((line = br.readLine()) != null) {
                 switch (LevelBuilder.numOfTabs(line)) {
                     case 0:
@@ -30,11 +34,29 @@ public class LevelBuilder {
                     case 2:
                         if (readBio) {
                             bio += line.substring(8);
+                        } else {
+                            picturePost = line.equals("        PicturePost:");
+                        }
+                        break;
+                    case 3:
+                        String firstNameLower = name.substring(0, 1).toLowerCase() + name.substring(1);
+                        if (picturePost) {
+                            if (photoLink == null) {
+                                photoLink = String.format(
+                                        "%s/timeline/%s", firstNameLower, line.substring(12)
+                                );
+                            } else {
+                                post = new PicturePost(
+                                        new Date(),
+                                        new Photo(photoLink, 300, line.substring(12))
+                                );
+                                photoLink = null;
+                            }
                         }
                         break;
                     default:
                         if (readBio) {
-                            String firstNameLower = name.substring(0, 1).toLowerCase() + name.substring(1);
+                            firstNameLower = name.substring(0, 1).toLowerCase() + name.substring(1);
                             persons.add(new Person(
                                     name,
                                     bio,
@@ -53,6 +75,8 @@ public class LevelBuilder {
                                     persons.get(persons.size() - 1).addPhoto(new Photo(photo.toString().substring(4)));
                                 }
                             }
+                        } else {
+                            persons.get(persons.size() - 1).getTimeline().addPost(post);
                         }
                 }
             }
