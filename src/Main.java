4b9e21222603b4e12b37ee057e12df861d85be18
Jake Goodman <jakeg135@gmail.com>
2018-06-07 05:53:21 -0400

    Merge

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,5 @@
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.util.ArrayList;
-
-public class Main extends Application {
-
-    public static Stage pStage;
-    public static Group root;
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        pStage = primaryStage;
-        root = new Group();
-
-        pStage.setTitle("Internet Investigator");
-        pStage.setScene(new Scene(root));
-        pStage.setHeight(550);
-        pStage.setWidth(500);
-        pStage.setResizable(false);
-
-        root.getChildren().add(new MainMenu());
-        /*root.getChildren().add(new Splashscreen());
-        root.getChildren().add(new Briefing1());*/
-
-        pStage.show();
-    }
-
-    public static void setStage(Scene s, int height, int width)
-    {
-        pStage.setScene(s);
-        pStage.setHeight(height);
-        pStage.setWidth(width);
-    }
-
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        classes.Runner.main(new String[]{});
     }
 }
